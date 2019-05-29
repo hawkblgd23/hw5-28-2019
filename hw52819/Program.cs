6d01260ca1a9b46787de2afd532820e651d361c4
hawkblgd23 <50380120+hawkblgd23@users.noreply.github.com>
2019-05-29 15:23:56 -0500

    Add files via upload

---
+++ hw52819/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace hw52819
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You got the big fish");
+            Console.WriteLine("Use your 50lb line");
+            Console.WriteLine("crapppie, bluegill, catfish");
+            string fish = Console.ReadLine();
+            switch (fish)
+            {
+                case "crappie":
+                    string text = Cool();
+                    Console.WriteLine(text);
+                    break;
+
+                case "bluegill":
+                    Junking();
+                    break;
+            } 
+        }
+        private static string Cool()
+        {
+            return "your got'em Sir!";
+        }
+        private static void Junking()
+        {
+            string[] baitlist = { "worms", "crayfish", "liver" };
+            for (int i = 0; i < baitlist.Length; i++)
+            {
+                Console.WriteLine(baitlist[i]);
+            } 
+        }
+    }
+}
